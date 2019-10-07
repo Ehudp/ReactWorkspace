597055a15344eabdc7358acb06096f94270ad3e6
ehudp <ehud.pinhas@gmail.com>
2019-10-07 16:05:28 +0300

    add set state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+// const App = () => {
+//   let index = 0;
+//   setInterval(() => {
+//     ++index;
+//     console.log(index);
+//   }, 1000);
+//   return <SeasonDisplay index={index} />;
+// };
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       index: 0
+//     };
+//     setInterval(() => {
+//       let { index } = this.state;
+//       console.log(index);
+//       this.setState({ index: ++index });
+//     }, 1000);
+//   }
+
+//   render() {
+//     return <SeasonDisplay index={this.state.index} />;
+//   }
+// }
 
 class App extends React.Component {
   render() {
@@ -10,4 +38,5 @@ class App extends React.Component {
     return <div>Latitude :</div>;
   }
 }
+
 ReactDOM.render(<App />, document.querySelector('#root'));
